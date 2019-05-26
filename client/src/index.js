ac57bd78387b02c86d7e13bfe40b34bb28761aab
Rob Howell <rob@robhowell.com>
2019-05-26 10:47:17 +0100

    Remove unnecessary config import

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
-import "./config.js";
 import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
